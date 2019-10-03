69dc9629c0be3a3746a0a81e1febbf8f40e00c39
TM <tolumark@TMs-MacBook-Pro.local>
2019-10-03 16:29:30 +0100

    Debugged

--- students/db/db.php
+++ students/db/db.php
@@ -10,7 +10,6 @@ $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 // Check connection
 if ($conn->connect_error) {
     //die("Connection failed: " . $conn->connect_error);
-	write_to_file($data);
     die("mysql FAIL");
 } 
 
