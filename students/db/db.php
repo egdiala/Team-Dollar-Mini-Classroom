c0c6531b3a8866406bcbaf3bb4a73a2294b7647e
egdiala <46764458+egdiala@users.noreply.github.com>
2019-10-04 17:52:03 +0100

    Update db.php

--- students/db/db.php
+++ students/db/db.php
@@ -5,12 +5,18 @@ $dbusername = "root";
 $dbpassword = "root";
 $dbname = "hngi_miniclassroom";
 
+
+$prodconfig = realpath($_SERVER['DOCUMENT_ROOT'] . "/../hngi-db-config.php");
+
+if (file_exists($prodconfig)) {
+    include $prodconfig;
+}
+
 // Create connection
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 // Check connection
 if ($conn->connect_error) {
     //die("Connection failed: " . $conn->connect_error);
     die("mysql FAIL");
-} 
-
+}
 ?>
